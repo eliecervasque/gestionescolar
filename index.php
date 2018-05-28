b61ba1c11a9744200c065197ddb1cb933a69f15c
Eliecer <eliecervasquez94@hotmail.com>
2018-05-28 15:17:50 -0500

    nueva version

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html ng-app="myApp">
+	<head>
+		<title>Plataforma Escolar</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<script type="text/javascript" src="js/jquery-3.3.1.slim.min.js"></script>
+		<script type="text/javascript" src="js/popper.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.7/angular.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.3/angular-route.js"></script>
+		<style type="text/css">
+			.logo{
+				background-image: url('img/ietaelsalado-escudo.png');
+				background-repeat: no-repeat;
+				background-size: 70% 80%;
+				background-position: center;
+				position: absolute;
+				background-color: #fff;
+				top: 0;
+				right: 40px;
+				width: 130px;
+				height: 130px;
+				box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+				z-index: 100;
+			}
+		</style>
+	</head>
+	<body>
+		<div role="button" tabindex="0" class="logo img-thumbnail" data-toggle="tooltip" data-placement="left" title="Institucion Educativa Tecnica Agropecuara El Salado"></div>
+		<div class="row bg-primary navbar">
+			<div class="col">Mensaje</div>
+			<div class="col">de prueba</div>
+		</div>
+	</body>
+</html>
